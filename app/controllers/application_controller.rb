6819c00267bef45227509a2e2094aa90abf54f56
tensyokuneko <npon.fafa@gmail.com>
2025-08-16 13:23:33 +0900

    Deviseのストロングパラメータを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 before_action :basic_auth
+before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -9,4 +10,9 @@ before_action :basic_auth
       username == 'admin' && password == '2222'
     end
   end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+  end 
 end
