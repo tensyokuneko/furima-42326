02c9eb240fa914b3ada25dd372bf0788d87c33b0
tensyokuneko <npon.fafa@gmail.com>
2025-08-23 11:23:36 +0900

    test: Itemモデルの単体テストを追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,20 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    category_id { 1 }
-    condition_id { 1 }
-    shipping_fee_id { 1 }
-    prefecture_id { 1 }
-    shipping_days_id { 1 }
-    user { nil }
-    price { 1 }
+    name              { "テスト商品" }
+    description       { "これはテスト用の商品です" }
+    category_id       { 2 }
+    condition_id      { 2 }
+    shipping_fee_id   { 2 }
+    prefecture_id     { 2 }
+    shipping_days_id  { 2 }
+    price             { 1000 }
+
+    # 出品者（Userモデル）との関連付け
+    association :user
+
+    # ActiveStorage 画像を添付
+    after(:build) do |item|
+      item.image.attach(io: File.open(Rails.root.join('spec/fixtures/files/test_image.png')), filename: 'test_image.png')
+    end
   end
 end
