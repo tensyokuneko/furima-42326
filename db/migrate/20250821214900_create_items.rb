845dc1a7bd3be92f238c4399f0850ff6ae312dff
tensyokuneko <npon.fafa@gmail.com>
2025-08-23 20:39:39 +0900

    マイグレーションにnull: falseを追加

--- db/migrate/20250821214900_create_items.rb
+++ db/migrate/20250821214900_create_items.rb
@@ -1,15 +1,16 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :description
-      t.integer :category_id
-      t.integer :condition_id
-      t.integer :shipping_fee_id
-      t.integer :prefecture_id
-      t.integer :shipping_days_id
+     t.string     :name, null: false
+      t.text       :description, null: false
+      t.integer    :category_id, null: false
+      t.integer    :condition_id, null: false
+      t.integer    :shipping_fee_id, null: false
+      t.integer    :prefecture_id, null: false
+      t.integer    :shipping_days_id, null: false
       t.references :user, null: false, foreign_key: true
-      t.integer :price
+      t.integer    :price, null: false
+
 
       t.timestamps
     end
