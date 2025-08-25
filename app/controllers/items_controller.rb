8becc81d97310f7b9c2a80b33ef076cb05c7a1b7
tensyokuneko <npon.fafa@gmail.com>
2025-08-26 06:32:40 +0900

    商品編集フォームのhogeを正しいカラム名とActiveHash参照に修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
