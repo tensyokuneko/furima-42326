5075721347e0b364c929186f9f85a78f73b9aa5f
tensyokuneko <npon.fafa@gmail.com>
2025-08-30 15:07:40 +0900

    購入フォームの配送先入力欄を正しい属性名に修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :index
+      render :index, status: :unprocessable_entity
     end
   end
 
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,17 @@ class OrderAddress
   attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number,
                 :user_id, :item_id, :token
 
-  # バリデーションはあとで追記します
+  with_options presence: true do
+    validates :post_code,     format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :city
+    validates :address
+    validates :phone_number,  format: { with: /\A\d{10,11}\z/ }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
