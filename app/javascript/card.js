c2da992aef49b7e3ca7f7fe0d83e1276cd4b0b57
tensyokuneko <npon.fafa@gmail.com>
2025-08-31 14:34:11 +0900

    不要なconsole.logを削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,9 @@
 const pay = () => {
- 
+  const numberElement = document.getElementById("number-form");
+  const expiryElement = document.getElementById("expiry-form");
+  const cvcElement = document.getElementById("cvc-form");
+
+  
 };
 
 window.addEventListener("turbo:load", pay);
