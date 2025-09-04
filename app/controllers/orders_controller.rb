65cc7768ca632f947104e88f05e705feb3ce9d44
tensyokuneko <npon.fafa@gmail.com>
2025-09-05 05:19:59 +0900

    PAY.JPライブラリの読み込みを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_address_params)
+      
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
