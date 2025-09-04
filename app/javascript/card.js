d1c4e648e00ca92999c0dca436b3fb6f3d757053
tensyokuneko <npon.fafa@gmail.com>
2025-09-05 05:42:53 +0900

    トークン生成処理とhiddenフィールドの追加を実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -15,6 +15,26 @@ const pay = () => {
   const cvc = elements.create("cardCvc");
   cvc.mount("#cvc-form");
 
+    const form = document.getElementById("charge-form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    payjp.createToken(number).then((response) => {
+      if (response.error) {
+        alert(response.error.message);
+      } else {
+        const token = response.id;
+
+        const tokenObj = `<input value="${token}" type="hidden" name="token">`;
+        form.insertAdjacentHTML("beforeend", tokenObj);
+      
+        
+      }
+    });
+  });
+
+
 
   
 };
