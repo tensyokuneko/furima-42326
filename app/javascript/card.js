abcac9304219ec242eb08228d50dc70578b634de
tensyokuneko <npon.fafa@gmail.com>
2025-09-05 05:24:56 +0900

    PAY.JPの公開鍵を設定し、カード情報欄を表示

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,6 +3,19 @@ const pay = () => {
   const expiryElement = document.getElementById("expiry-form");
   const cvcElement = document.getElementById("cvc-form");
 
+  const payjp = Payjp('pk_test_0438cdf36f0cfef1388612d7');
+  const elements = payjp.elements();
+
+  const number = elements.create("cardNumber");
+  number.mount("#number-form");
+
+  const expiry = elements.create("cardExpiry");
+  expiry.mount("#expiry-form");
+
+  const cvc = elements.create("cardCvc");
+  cvc.mount("#cvc-form");
+
+
   
 };
 
