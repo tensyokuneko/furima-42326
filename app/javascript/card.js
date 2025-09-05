dc0b74326a2c0c0dcf08ace28847d5a5e6eebf63
tensyokuneko <npon.fafa@gmail.com>
2025-09-06 06:18:44 +0900

    公開鍵をgon経由で取得するようにcard.jsを修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,7 @@ const pay = () => {
   const expiryElement = document.getElementById("expiry-form");
   const cvcElement = document.getElementById("cvc-form");
 
-  const payjp = Payjp('pk_test_0438cdf36f0cfef1388612d7');
+  const payjp = Payjp(gon.public_key); 
   const elements = payjp.elements();
 
   const number = elements.create("cardNumber");
